cfb06692a8bdb9d31843470a8c4e6bccbd96d823
Jakub Pliszka <jakubpliszka@outlook.com>
2022-08-02 23:27:50 +0200

    Added stock prediction and visualization of the results

--- rnn.py
+++ rnn.py
@@ -9,6 +9,8 @@ from dataset import prepare_dataset
 def run_rnn() -> None:
     x_train, y_train, scaler, dataset_train, dataset_test = prepare_dataset()
     rnn = build_model(x_train, y_train)
+    predicted_stock_price = predict(scaler, rnn, dataset_train, dataset_test)
+    visualize_results(dataset_test, predicted_stock_price)
 
 
 def build_model(x_train: np.ndarray, y_train: np.ndarray) -> tf.keras.models.Sequential():
@@ -31,4 +33,33 @@ def build_model(x_train: np.ndarray, y_train: np.ndarray) -> tf.keras.models.Seq
     rnn.fit(x_train, y_train, epochs=50, batch_size=32)
     return rnn
 
+
+def predict(scaler, rnn, dataset_train, dataset_test):
+    dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0)
+
+    inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+    inputs = inputs.reshape(-1, 1)
+    inputs = scaler.transform(inputs)
+
+    x_test: list = []
+    for i in range(60, len(dataset_test)):
+        x_test.append(inputs[i - 60:i, 0])
+
+    x_test = np.array(x_test)
+    input_shapes = (x_test.shape[0], x_test.shape[1], 1)
+    x_test = np.reshape(x_test, input_shapes)
+
+    predicted_stock_price = rnn.predict(x_test)
+    predicted_stock_price = scaler.inverse_transform(predicted_stock_price)
+    return predicted_stock_price
+
+
+def visualize_results(real_stock_price, predicted_stock_price) -> None:
+    plt.plot(real_stock_price, color='red', label='Real Stock Price')
+    plt.plot(predicted_stock_price, color='blue', label='Predicted  Stock Price')
+    plt.title('Stock Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel('Stock Price')
+    plt.legend()
+    plt.show()
 
